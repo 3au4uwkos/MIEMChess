83b75596659c23eb439c9aa1990861433c2fe26a
Matvei Zaitsev <3au4uwkos@gmail.com>
2025-03-29 09:40:12 +0300

    Контроллеры переписаны в реактивной парадигме

--- src/main/java/io/github/_au4uwkos/chess_game/controller/AuthenticationController.java
+++ src/main/java/io/github/_au4uwkos/chess_game/controller/AuthenticationController.java
@@ -2,45 +2,40 @@ package io.github._au4uwkos.chess_game.controller;
 
 import io.github._au4uwkos.chess_game.model.Role;
 import io.github._au4uwkos.chess_game.model.UserEntity;
+import io.github._au4uwkos.chess_game.model.UserRole;
 import io.github._au4uwkos.chess_game.repository.RoleRepository;
 import io.github._au4uwkos.chess_game.repository.UserRepository;
 import io.github._au4uwkos.chess_game.security.JWTGenerator;
 import io.github._au4uwkos.chess_game.transfer.AuthResponseTransfer;
 import io.github._au4uwkos.chess_game.transfer.LoginTransfer;
 import io.github._au4uwkos.chess_game.transfer.RegisterTransfer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 import java.util.Collections;
 
-@Controller
+@RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
 
-    private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final JWTGenerator jwtGenerator;
 
-    @Autowired
-    public AuthenticationController(AuthenticationManager authenticationManager,
-                                    UserRepository userRepository,
-                                    RoleRepository roleRepository,
-                                    PasswordEncoder passwordEncoder,
-                                    JWTGenerator jwtGenerator) {
-        this.authenticationManager = authenticationManager;
+    public AuthenticationController(
+            UserRepository userRepository,
+            RoleRepository roleRepository,
+            PasswordEncoder passwordEncoder,
+            JWTGenerator jwtGenerator
+    ) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
@@ -48,36 +43,41 @@ public class AuthenticationController {
     }
 
     @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AuthResponseTransfer> login(@RequestBody LoginTransfer loginTransfer) {
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginTransfer.getUsername(),
-                        loginTransfer.getPassword()
-                )
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String token = jwtGenerator.generateToken(authentication);
-        return new ResponseEntity<>(new AuthResponseTransfer(token), HttpStatus.OK);
-
+    public Mono<ResponseEntity<AuthResponseTransfer>> login(@RequestBody LoginTransfer loginTransfer) {
+        return userRepository.findByUsername(loginTransfer.getUsername())
+                .flatMap(user -> {
+                    Authentication authentication = new UsernamePasswordAuthenticationToken(
+                            user.getUsername(), loginTransfer.getPassword()
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    String token = jwtGenerator.generateToken(authentication);
+                    return Mono.just(ResponseEntity.ok(new AuthResponseTransfer(token)));
+                })
+                .switchIfEmpty(Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()));
     }
 
+    @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<ResponseEntity<String>> register(@RequestBody RegisterTransfer registerTransfer) {
+        return userRepository.existsByUsername(registerTransfer.getUsername())
+                .flatMap(exists -> {
+                    if (exists) {
+                        return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username is taken!"));
+                    }
 
-    @PostMapping("register")
-    public ResponseEntity<String> register(@RequestBody RegisterTransfer registerTransfer) {
-        if (userRepository.existsByUsername(registerTransfer.getUsername())) {
-            return new ResponseEntity<>("Username is taken!", HttpStatus.BAD_REQUEST);
-        }
+                    UserEntity user = new UserEntity();
+                    user.setUsername(registerTransfer.getUsername());
+                    user.setPassword(passwordEncoder.encode(registerTransfer.getPassword()));
 
-        UserEntity user = new UserEntity();
-        user.setUsername(registerTransfer.getUsername());
-        user.setPassword(passwordEncoder.encode((registerTransfer.getPassword())));
+                    return roleRepository.findByName("USER")
+                            .flatMap(role -> {
 
-        @SuppressWarnings("OptionalGetWithoutIsPresent") Role roles = roleRepository.findByName("USER").get();
-        user.setRoles(Collections.singletonList(roles));
+                                UserRole userRole = new UserRole(user.getId(), role.getId());
 
-        userRepository.save(user);
-
-        return new ResponseEntity<>("User register successful!", HttpStatus.OK);
+                                user.setRoles(Collections.singletonList(userRole)); // Теперь это List<UserRole>
+                                return userRepository.save(user);
+                            })
+                            .thenReturn(ResponseEntity.ok("User register successful!"));
+                });
     }
 }
+
--- src/main/java/io/github/_au4uwkos/chess_game/model/UserRole.java
+++ src/main/java/io/github/_au4uwkos/chess_game/model/UserRole.java
@@ -1,9 +1,12 @@
 package io.github._au4uwkos.chess_game.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.annotation.Id;
 
 @Table("user_roles")
+@Data
 public class UserRole {
 
     @Id
