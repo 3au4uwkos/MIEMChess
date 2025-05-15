9119e01791f38e6a5b7a17906b3a20954d999c49
suhrobdomoiZ <avdeevmatvey06@gmail.com>
2025-05-15 15:06:49 +0300

    Добавление новых фич из фронта,

--- src/main/java/io/github/_au4uwkos/chess_game/repository/UserRepository.java
+++ src/main/java/io/github/_au4uwkos/chess_game/repository/UserRepository.java
@@ -4,9 +4,10 @@ import io.github._au4uwkos.chess_game.model.UserEntity;
 
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-
+@Component
 public interface UserRepository extends ReactiveCrudRepository<UserEntity, Integer> {
     @Query("SELECT * FROM users WHERE username = :username")
     Mono<UserEntity> findByUsername(String username);
